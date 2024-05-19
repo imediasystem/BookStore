f2c1faba8235d369622e2c207c2d29bd4e36caa6
iMedia SysteM <imediasystem@hotmail.com>
2024-05-19 13:31:15 +0200

    Add project files.

---
+++ BookStore.BL/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.BL
+{
+    internal class Book
+    {
+        string title;
+        string authorname;
+        string authorsurname;
+        int pagesNumber;
+
+        string GetLabel()
+        {
+            return title + " " + authorname + " " + authorsurname;
+        }
+    }
+}
